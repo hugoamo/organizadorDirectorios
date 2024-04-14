142087d8ad76ecd898df2b1b35e9da10c3de0eb5
hugoamo <hamellizo8701@gmail.com>
2024-04-13 22:32:06 -0500

    Generar ejecutable

---
+++ organizador.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'organizador.exe' with Python DLL being found.
+set PATH=C:\PROGRA~1\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Program Files\Python311
+set NUITKA_PYTHONPATH=D:\USUARIO\Documents\Repositorios\python\ordenadorFiles;C:\Program Files\Python311\DLLs;C:\Program Files\Python311\Lib;C:\Program Files\Python311;D:\USUARIO\Documents\Repositorios\python\ordenadorFiles\venv;D:\USUARIO\Documents\Repositorios\python\ordenadorFiles\venv\Lib\site-packages
+"%~dp0organizador.exe" %*
